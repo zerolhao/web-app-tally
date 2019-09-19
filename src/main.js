5a74232f98248d9b6d5bdb94be8f2714d7760dd5
zerolhao <zerolhao@gmail.com>
2019-09-19 17:46:46 +0800

    全局自动注册基础组件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,47 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import './assets/css/reset.css'
 import './assets/css/global.less'
 
 Vue.config.productionTip = false
+
+// 全局注册基础组件
+const requireComponent = require.context(
+  // 其组件目录的相对路径
+  './components',
+  // 是否查询其子目录
+  false,
+  // 匹配基础组件文件名的正则表达式
+  /base-[a-z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  // 获取组件配置
+  const componentConfig = requireComponent(fileName)
+
+  // 获取组件的 PascalCase 命名
+  const componentName = upperFirst(
+    camelCase(
+      // 获取和目录深度无关的文件名
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  // 全局注册组件
+  Vue.component(
+    componentName,
+    // 如果这个组件选项是通过 `export default` 导出的，
+    // 那么就会优先使用 `.default`，
+    // 否则回退到使用模块的根。
+    componentConfig.default || componentConfig
+  )
+})
 
 new Vue({
   router,
