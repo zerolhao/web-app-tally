72a179bc12f65f2b834c858873de1e2aeb584c7d
zerolhao <zerolhao@gmail.com>
2019-09-19 17:47:16 +0800

    添加基础组件-icon

---
+++ src/components/base-icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    class="icon base-icon"
+    aria-hidden="true"
+    :style="{ color, 'font-size': size }"
+  >
+    <use :href="'#' + name"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'base-icon',
+  props: {
+    name: String,
+    color: String,
+    size: String
+  }
+}
+</script>
+<style lang="less">
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  color: #455a64;
+}
+</style>
